1c82e9bde42db3e6b0936d284c8097dd115eef5d
Sharad Kumar Singh <Singhsharad529@gmail.com>
2020-06-01 16:53:09 +0530

    Create program1.py

---
+++ Day1/program1.py
@@ -0,0 +1,9 @@
+print("Please Enter Details given below")
+name=input("Name:\n")
+branch=input("Branch:\n")
+gender=input("Gender:\n")
+college=input("College Name:\n")
+age=int(input("Age:\n"))
+
+print("\nHere is your details")
+print("Name:",name,"\nBranch:",branch,"\nGender:",gender,"\nCollege Name:",college,"\nAge:",age)
