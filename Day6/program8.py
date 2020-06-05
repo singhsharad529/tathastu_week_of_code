01d927e5843acd592db3fce0fea5815ec71cb70f
Sharad Kumar Singh <Singhsharad529@gmail.com>
2020-06-05 17:22:21 +0530

    Create program8.py

---
+++ Day6/program8.py
@@ -0,0 +1,47 @@
+ 
+def spiralPrint(m, n, a) : 
+    k = 0; l = 0 
+    while (k < m and l < n) : 
+  
+        for i in range(l, n) : 
+            print(a[k][i], end = " ") 
+              
+        k += 1
+  
+        for i in range(k, m) : 
+            print(a[i][n - 1], end = " ") 
+              
+        n -= 1
+   
+        if ( k < m) : 
+              
+            for i in range(n - 1, (l - 1), -1) : 
+                print(a[m - 1][i], end = " ") 
+              
+            m -= 1
+   
+        if (l < n) : 
+            for i in range(m - 1, k - 1, -1) : 
+                print(a[i][l], end = " ") 
+              
+            l += 1
+  
+
+
+R=int(input("enter number of row :"))
+C=int(input("enter number of column"))
+number = 1
+matrix1=[]
+ 
+for i in range(R):
+    matrix2 = []
+    for j in range(C):
+        matrix2.append(number)
+        number += 1
+    matrix1.append(matrix2)
+ 
+print('your matrix:\n',matrix1)
+print('spiral print:\n')
+spiralPrint(R, C, matrix1) 
+  
+ 
